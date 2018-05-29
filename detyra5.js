19568f67a13e0c45d8ac82c3ad09c384e616a05d
MikeaMihali <mikea.mihali@fshnstudent.info>
2018-05-29 20:34:50 +0200

    deytyra5

---
+++ detyra5.js
@@ -0,0 +1,63 @@
+
+var center = {lat: 41.3190, lng: 19.8188};
+var mapOptions={
+      center:center,
+      zoom:15
+  };
+function initMap(){
+    var map=new google.maps.Map(document.getElementById("map"),mapOptions);
+    downloadUrl('http://localhost/gis.php',function(data){
+    var xml = data.responseXML;
+    var hotels = xml.documentElement.getElementsByTagName('hotel');
+    Array.prototype.forEach.call(hotels,function(hotel){
+        var id=hotel.getAttribute('id');
+        var name=hotel.getAttribute('name');
+        var value=hotel.getAttribute('value');
+        var point=new google.maps.LatLng(
+            parseFloat(hotel.getAttribute('lat')),
+            parseFloat(hotel.getAttribute('lng'))
+        );
+        console.log(point);
+        var marker = new google.maps.Circle({
+            strokeColor: '#ffffff',
+            strokeOpacity: 1,
+            strokeWeight: 2,
+            fillColor: '##8383fc',
+            fillOpacity: 0.35,
+            map: map,
+            center:point,
+            radius:parseFloat(value)
+          });
+          var infowindow=new google.maps.InfoWindow;
+          infowindow.setContent("Name :"+name+"\n"+"value :"+value);
+          infowindow.setPosition(point);
+          infowindow.open(map);
+    })
+    })
+}
+function downloadUrl(url, callback) {
+    var request = window.ActiveXObject ?
+        new ActiveXObject('Microsoft.XMLHTTP') :
+        new XMLHttpRequest;
+
+    request.onreadystatechange = function() {
+      if (request.readyState == 4) {
+        request.onreadystatechange = doNothing;
+        callback(request, request.status);
+      }
+    };
+
+    request.open('GET', url, true);
+    request.send(null);
+  }
+function doNothing() {}
+$(document).ready(function(){
+    $("#arrow").click(function(){
+        $("#info-div").hide(1000);
+     });
+     $("#arrow-next").click(function(){
+        $("#info-div").show(1000);
+     });
+     
+     $("#arrow").click();
+});
---
+++ gis.php
@@ -0,0 +1,44 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$database="test";
+$conn=mysqli_connect($servername,$username,$password,$database);
+if ($conn->connect_error) {
+	die("Connectio failed".$conn->connect_error);
+}
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+
+$query="SELECT * FROM geoinfo WHERE 1";
+$results=mysqli_query($conn,$query);
+if (!$results) {
+    die('Invalid query: ' . mysql_error());
+  }
+header("Content-type: text/xml");
+echo "<?xml version='1.0' ?>";
+echo '<hotels>';
+$ind=0;
+// Iterate through the rows, printing XML nodes for each
+while ($row = mysqli_fetch_array($results,MYSQLI_ASSOC)){
+  // Add to XML document node
+  echo '<hotel ';
+  echo 'id="' . $row['ID'] . '" ';
+  echo 'name="' . parseToXML($row['name']) . '" ';
+  echo 'lat="' . $row['lat'] . '" ';
+  echo 'lng="' . $row['lng'] . '" ';
+  echo 'value="' . $row['value'] . '" ';
+  echo '/>';
+  $ind = $ind + 1;
+}
+
+// End XML file
+echo '</hotels>';
+?>
